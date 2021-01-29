5e39d7bc8f563cf218372839ef6dc573c38ae5d5
mohamaditani <62497623+mohamaditani@users.noreply.github.com>
2021-01-29 15:26:41 +0200

    creating basic html glass look and not functional JS

---
+++ transparent.js
@@ -0,0 +1,16 @@
+
+// 
+// var i = 5;
+// function myLoop() {
+//   setTimeout(function() {
+//     $("#c1").css("top", i + "%");
+//     i += 0.1;
+//     if (i < 75) {
+//       myLoop();
+//     }
+//   }, i)
+// }
+//
+//
+// //myFunction();
+// window.onload = myLoop();
