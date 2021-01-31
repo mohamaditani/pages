fe84baf3eedfac88e3c2e812eb0efbfc0049e211
mohamaditani <62497623+mohamaditani@users.noreply.github.com>
2021-01-31 10:52:40 +0200

    add the cursor using js

--- transparent.js
+++ transparent.js
@@ -1,5 +1,4 @@
-
-// 
+//
 // var i = 5;
 // function myLoop() {
 //   setTimeout(function() {
@@ -14,3 +13,38 @@
 //
 // //myFunction();
 // window.onload = myLoop();
+
+// function myMove(circle) {
+//   var elem = document.getElementById("circle");
+//   var pos = 0;
+//   var i = 6;
+//   var id = setInterval(frame, i);
+//   function frame() {
+//     if (pos == 600) {
+//       clearInterval(id);
+//     } else {
+//       pos++;
+//       elem.style.top = pos + "px";
+//       elem.style.left = pos + "px";
+//     }
+//   }
+// }
+//
+// window.onload = myMove(c1);
+
+$(document).on("mousemove", function(event) {
+
+  const mouseX = event.pageX;
+  const mouseY = event.pageY;
+
+  //const ball = $(document).find("#ball");
+
+  $('div.ball').css({
+    'left': mouseX +'px',
+    'top': mouseY +'px'
+  });
+});
+// $("#ball").css("top", "100%");
+// $("ball").css.style.left = mouseX + "px";
+// $("ball").css.style.right = mouseY + "px";
+// });
